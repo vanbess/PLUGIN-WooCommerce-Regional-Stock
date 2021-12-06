37c9c3d12f569713b7ffa08516050ec45fbe1386
vanbess <dev@silverbackdev.co.za>
2021-12-06 15:13:50 +0200

    Switched method of displaying products as in stock

--- classes/SBWC_RS.php
+++ classes/SBWC_RS.php
@@ -40,6 +40,9 @@ class SBWC_RS
 
         // filter stock status strings based on custom defined RS stock status strings
         add_filter('woocommerce_get_availability', [__CLASS__, 'rs_display_custom_stock_status'], 1, 2);
+
+        // action to set correct class for stock status text and add to cart button
+        add_action('woocommerce_before_variations_form', [__CLASS__, 'rs_disable_atc_bn']);
     }
 
     /**
@@ -107,7 +110,7 @@ class SBWC_RS
                 <p class="rs_form_container">
                     <!-- label -->
                     <label for="rs_countries rs_prod_ids">
-                        <?php _e('Specify all country codes and associated variations for which you would like to set stock status to out of stock:', 'woocommerce'); ?>
+                        <?php _e('Specify all country codes and associated variations for which you would <u><b>like to show a stock status of in stock:</b></u>', 'woocommerce'); ?>
                     </label>
                 </p>
 
@@ -160,12 +163,12 @@ class SBWC_RS
 
                     <!-- save countries -->
                     <button id="rs_save_countries" class="button button-primary button-large" data-nonce="<?php echo wp_create_nonce('rs save countries'); ?>">
-                        <?php _e('Save Countries', 'woocommerce'); ?>
+                        <?php _e('Save Regional Stock Avialability', 'woocommerce'); ?>
                     </button>
 
                     <!-- remove all stock restrictions -->
                     <button id="rs_remove_restrictions" class="button button-secondary button-large" data-children="<?php echo implode(',', $children); ?>" data-nonce="<?php echo wp_create_nonce('rs save countries'); ?>">
-                        <?php _e('Remove all regional stock availability restrictions', 'woocommerce'); ?>
+                        <?php _e('Reset Regional Stock Availability', 'woocommerce'); ?>
                     </button>
 
                 </p>
@@ -232,12 +235,12 @@ class SBWC_RS
 
                     <!-- save countries -->
                     <button id="rs_save_text" class="button button-primary button-large" data-nonce="<?php echo wp_create_nonce('rs save countries'); ?>">
-                        <?php _e('Save Custom Text', 'woocommerce'); ?>
+                        <?php _e('Save Custom Out Of Stock Text', 'woocommerce'); ?>
                     </button>
 
                     <!-- remove custom text -->
                     <button id="rs_remove_text" class="button button-secondary button-large" data-children="<?php echo implode(',', $children); ?>" data-nonce="<?php echo wp_create_nonce('rs save countries'); ?>">
-                        <?php _e('Remove all custom out of stock text', 'woocommerce'); ?>
+                        <?php _e('Remove All Custom Out Of Stock Text', 'woocommerce'); ?>
                     </button>
 
                 </p>
@@ -246,7 +249,7 @@ class SBWC_RS
 
         </div>
 
-<?php
+    <?php
         // css and js
         wp_enqueue_style('rs-css');
         wp_enqueue_style('rs-js');
@@ -269,20 +272,97 @@ class SBWC_RS
         // retrieve custom stock text
         $rs_stock_status = get_post_meta($prod_id, 'rs_text', true);
 
-        // retrieve excluded countries string
-        $rs_excluded_countries = get_post_meta( $prod_id, 'rs_countries', true );
+        // retrieve included countries string
+        $instock_countries = explode(',', get_post_meta($prod_id, 'rs_countries', true));
 
         // retrieve user location
         $location = WC_Geolocation::geolocate_ip();
         $country = $location['country'];
 
-        // set custom stock status string if custom string exists AND user country code found in country code string
-        if ($rs_stock_status && strpos($rs_excluded_countries, $country) !== false) :
-            $availability['availability'] = __($rs_stock_status, 'woocommerce');
+        // If a country is not present in $instock_countries array, mark items as out of stock.
+        if (!in_array($country, $instock_countries)) :
+            if ($rs_stock_status) :
+                $availability['availability'] = __($rs_stock_status, 'woocommerce');
+            else :
+                $availability['availability'] = __('Out of stock', 'woocommerce');
+            endif;
         endif;
 
         return $availability;
     }
+
+    /**
+     * Set stock text class and disable add to cart button as needed
+     *
+     * @return void
+     */
+    public static function rs_disable_atc_bn()
+    {
+        // retrieve product object
+        global $post;
+        $parent_id = $post->ID;
+        $prod_obj = wc_get_product($parent_id);
+
+        // if product does not have child, bail early
+        if ($prod_obj->has_child() === false) :
+            return;
+        endif;
+
+        // retrieve children
+        $children = $prod_obj->get_children();
+
+        // array to hold relevant (regionally available) product ids
+        $rs_products = [];
+
+        // retrieve user location
+        $location = WC_Geolocation::geolocate_ip();
+        $country = $location['country'];
+
+        // if current user country present in list of countries for any child, push to $rs_products
+        foreach ($children as $cid) :
+            if (get_post_meta($cid, 'rs_countries', true)) :
+
+                $instock_countries  = explode(',', get_post_meta($cid, 'rs_countries', true));
+
+                if (in_array($country, $instock_countries)) :
+                    $rs_products[] = $cid;
+                endif;
+
+            endif;
+        endforeach;
+
+        // if $rs_products is empty, bail once again
+        if (empty($rs_products)) :
+            return;
+        endif;
+    ?>
+        <input type="hidden" id="rs_in_stock" value="<?php echo implode(',', $rs_products); ?>">
+        <script>
+            jQuery(document).ready(function($) {
+
+                $('.swatchinput').on('click', function() {
+
+                    setTimeout(function() {
+
+                        let variation_id = $('.variation_id').val();
+                        let rs_in_stock = $('#rs_in_stock').val();
+                        let position = rs_in_stock.indexOf(variation_id);
+
+                        if (position === -1) {
+                            $('.single_add_to_cart_button').addClass('disabled wc-variation-is-unavailable');
+                            $('.stock').removeClass('in-stock').addClass('out-of-stock');
+                        } else if (position === 0) {
+                            $('.single_add_to_cart_button').removeClass('disabled wc-variation-is-unavailable');
+                            $('.stock').removeClass('out-of-stock').addClass('in-stock');
+                        }
+
+                    }, 100);
+
+                });
+
+            });
+        </script>
+<?php }
 }
 
 SBWC_RS::init();
